c4dca5e934b06a1a47ea52ab35721a05068e79b3
Serge Rosmorduc <devnull@localhost>
2010-11-02 13:02:12 +0100

    (removing some unused classes)

--- test/jsesh/jhotdraw/MyOSXPaletteHandler.java
+++ test/jsesh/jhotdraw/MyOSXPaletteHandler.java
@@ -14,11 +14,17 @@
 
 package jsesh.jhotdraw;
 
-import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
-import org.jhotdraw.app.OSXApplication;
-import org.jhotdraw.app.View;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jhotdraw_7_4_1.app.View;
 /**
  * Hides all registered floating palettes, if none of the registered view
  * windows have focus anymore.
