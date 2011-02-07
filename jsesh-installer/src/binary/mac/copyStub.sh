facbdff1356574f26af36ddb7c5a39d0ee52fc4d
Serge Rosmorduc <devnull@localhost>
2011-02-07 10:14:51 +0100

    improving mac app creation

--- jsesh-installer/src/binary/mac/copyStub.sh
+++ jsesh-installer/src/binary/mac/copyStub.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
+
 mkdirIfNotE () {
     if [ ! -d "$1" ] ; then
 	mkdir "$1"
     fi
 }
 
-# ok, we need to fix the Info.plist files a bit... we want them
-# to contain a list
-# of strings like <string>$APP_PACKAGE/../lib/antlr-2.7.7.jar</string>
-# where the original files contain <MY_CLASSPATH/>
+# prepare an application.
+#
+prepareApp() {
+    app="$1"
+    head="Info.plist.$app"
+    plistFile="$dest/$app.app/Contents/Info.plist"
+    cat  "$head" /tmp/dep_list.txt Info.plist.end > "$plistFile"
+
+    # Actual stub copy...
+    # Avoid any License problems...
+    stub=/System/Library/Frameworks/JavaVM.framework/Resources/MacOS/JavaApplicationStub
+
+    mkdirIfNotE "$dest/$app.app/Contents/MacOS"
+    cp $stub "$dest/$app.app/Contents/MacOS"
+
+    # cleanup
+}
 
 
 dest="$1"
 cd "$dest"
 
+
+# ok, we need to fix the Info.plist files a bit... we want them
+# to contain a list
+# of strings like <string>$APP_PACKAGE/../lib/antlr-2.7.7.jar</string>
+# where the original files contain <MY_CLASSPATH/>
+
 ls ./lib/ | sed 's%\(.*\)%<string>$APP_PACKAGE/../lib/\1</string>%' >/tmp/dep_list.txt
 
-cat  Info.plist.jsesh /tmp/dep_list.txt Info.plist.end > "$dest/JSesh.app/Contents/Info.plist"
+prepareApp JSesh
 
 # cleanup
 #rm Info.plist.end Info.plist.jsesh
 
 
-# Actual stub copy...
-# Avoid any License problems...
-stub=/System/Library/Frameworks/JavaVM.framework/Resources/MacOS/JavaApplicationStub
 
-mkdirIfNotE "$dest/JSesh.app/Contents/MacOS"
-cp $stub "$dest/JSesh.app/Contents/MacOS"
 
