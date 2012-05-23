50b23b888506165f4583c1d5b35fce3300c989aa
Serge Rosmorduc <devnull@localhost>
2012-05-23 15:57:57 +0200

    toward new drawing system...

---
+++ jsesh/src/main/java/jsesh/newEdit/NewMDCEditor.java
@@ -0,0 +1,19 @@
+package jsesh.newEdit;
+
+import org.qenherkhopeshef.viewToolKit.JGraphicalElementDisplayer;
+import org.qenherkhopeshef.viewToolKit.drawing.PlainDrawing;
+
+/**
+ * New variant of the Manuel de codage editor class.
+ * @author Serge Rosmorduc (serge.rosmorduc@qenherkhopeshef.org)
+ *
+ */
+@SuppressWarnings("serial")
+public class NewMDCEditor extends JGraphicalElementDisplayer<PlainDrawing>{
+
+	public NewMDCEditor() {
+		super(new PlainDrawing());
+	}
+
+	
+}
