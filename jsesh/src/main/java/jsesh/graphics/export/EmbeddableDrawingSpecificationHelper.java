770fe4eb736b3dc1620e9d5bb59b2f1b7eb35013
Serge Rosmorduc <serge.rosmorduc@qenherkhopeshef.org>
2019-02-27 17:20:30 +0100

    factorized code for creating marginless drawing specifications

---
+++ jsesh/src/main/java/jsesh/graphics/export/EmbeddableDrawingSpecificationHelper.java
@@ -0,0 +1,23 @@
+package jsesh.graphics.export;
+
+import jsesh.mdcDisplayer.preferences.DrawingSpecification;
+import jsesh.mdcDisplayer.preferences.DrawingSpecificationsImplementation;
+import jsesh.mdcDisplayer.preferences.PageLayout;
+
+import java.awt.*;
+
+/**
+ * Helper class for creating drawing specifications for embedded pictures.
+ */
+
+public class EmbeddableDrawingSpecificationHelper {
+
+	public static DrawingSpecification createEmbeddedDrawingSpecifications(DrawingSpecification originalSpecifications) {
+		DrawingSpecification newDrawingSpecifications = originalSpecifications.copy();
+        PageLayout pageLayout = newDrawingSpecifications.getPageLayout();
+        pageLayout.setLeftMargin(01);
+        pageLayout.setTopMargin(01);
+        newDrawingSpecifications.setPageLayout(pageLayout);
+        return newDrawingSpecifications;
+	}
+}
