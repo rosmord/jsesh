bf56af3d7c181ca7fac1ef5ca8108b4654471de8
Serge Rosmorduc <serge.rosmorduc@qenherkhopeshef.org>
2019-07-20 15:43:27 +0200

    work on improved searches

---
+++ jsesh/src/main/java/jsesh/search/simpleSearch/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * A slightly improved search system.
+ *
+ * <p><Allows:</p>
+ * <ul>
+ *     <li>Wildcards '*'</li>
+ *     <li>size limitation</li>
+ *     <li>search for variant signs</li>
+ * </ul>
+ */
+package jsesh.search.simpleSearch;
