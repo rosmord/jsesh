93f1b1f30c8d21cc356eeabba17dfb2ff654f1bc
Serge ROSMORDUC <serge.rosmorduc@cnam.fr>
2020-05-22 10:24:50 +0200

    in work...

--- jsesh/src/main/java/jsesh/hieroglyphs/data/HieroglyphDatabaseInterface.java
+++ jsesh/src/main/java/jsesh/hieroglyphs/data/HieroglyphDatabaseInterface.java
@@ -1,10 +1,14 @@
 /*
- * NewInterface.java
- * 
+ * Copyright ou © ou Copr. Serge Rosmorduc (2004-2020) 
+ * serge.rosmorduc@cnam.fr
+
+ * Ce logiciel est régi par la licence CeCILL-C soumise au droit français et
+ * respectant les principes de diffusion des logiciels libres : "http://www.cecill.info".
+
+ * This software is governed by the CeCILL-C license 
+ * under French law : "http://www.cecill.info". 
+ *
  * Created on 27 sept. 2007, 17:18:09
- * 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
  */
 package jsesh.hieroglyphs.data;
 
